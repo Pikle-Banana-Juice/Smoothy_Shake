ca81c61313eae0a2bdb92e56e46b93c283bad7f3
Pikle-Banana-Juice <thomas.joyeux@le-campus-numerique.fr>
2024-08-12 15:58:10 +0200

    Add files via upload

--- spark.py
+++ spark.py
@@ -1,2 +1,8 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("NomDeVotreApplication") \
+    .getOrCreate()
+
 df = spark.read.json("cvelistV5/cves/2024/0xxx/CVE-2024-0006.json")
 df.where("product == YugabyteDB Anywhere").show()
